268cf197a86adf092c08ef5b69e0870480d2dbfa
Matthieu Playe <matpl1996@outlook.com>
2023-11-19 11:59:18 +0100

    Add basic components and styles

--- portfolio/src/app/pages/home.page/home.page.component.ts
+++ portfolio/src/app/pages/home.page/home.page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HeroComponent} from "../../components/hero/hero.component";
 
 @Component({
   selector: 'app-home.page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeroComponent],
   templateUrl: './home.page.component.html',
   styleUrl: './home.page.component.scss'
 })
