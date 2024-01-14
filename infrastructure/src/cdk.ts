de8583192f8951486403b09ac3df9deb25179d7e
Matthieu Playe <matpl1996@outlook.com>
2024-01-14 14:44:54 +0100

    Change cdk name

--- infrastructure/src/cdk.ts
+++ infrastructure/src/cdk.ts
@@ -1,8 +1,9 @@
 import { App } from "aws-cdk-lib";
 import {CdkDistribution} from "./stacks/cdk-distribution";
+import {environment} from "../../shared/environment/environment";
 
 const app = new App();
-new CdkDistribution(app, "CdkDistribution", {
+new CdkDistribution(app,  environment.productName + "-Distribution", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
