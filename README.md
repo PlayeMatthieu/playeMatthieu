8fb01f4d9b80ed3c9d78aa92b8d9155cdba05911
Matthieu Playe <matpl1996@outlook.com>
2024-03-16 19:59:29 +0100

    Remove current website design

---
+++ README.md
@@ -0,0 +1,11 @@
+# Portfolio project
+#### Angular 17, AWS CDK
+
+## How to deploy to aws
+1. Install aws cdk
+2. Install aws cli
+3. Configure aws cli
+4. Run `npm run build`
+5. Run `cdk deploy`
+6. Go to aws console and check the deployed stack
+7. Run `cdk destroy` to destroy the stack
--- portfolio/src/app/pages/home.page/home.page.component.ts
+++ portfolio/src/app/pages/home.page/home.page.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HeroComponent} from "../../components/hero/hero.component";
-import {AboutComponent} from "../../components/about/about.component";
-import {ProjectsComponent} from "../../components/projects/projects.component";
-import {ExperienceSkillsComponent} from "../../components/experience-skills/experience-skills.component";
-import {ContactComponent} from "../../components/contact/contact.component";
 import {SkillsSliderComponent} from "../../components/skills-slider/skills-slider.component";
-import {ServicesComponent} from "../../components/services/services.component";
 
 @Component({
   selector: 'app-home.page',
   standalone: true,
-  imports: [CommonModule, HeroComponent, AboutComponent, ProjectsComponent, ExperienceSkillsComponent, ContactComponent, SkillsSliderComponent, ServicesComponent],
+  imports: [CommonModule,SkillsSliderComponent],
   templateUrl: './home.page.component.html',
   styleUrl: './home.page.component.scss'
 })
